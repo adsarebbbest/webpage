327a173cb3bac5d13f67a5fe0872698e2b38aeaf
blob <zzjjyy179@gmail.com>
2021-01-13 18:11:56 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "<!DOCTYPE html><html><head><title>Hello</title></head><body><p>Hello World</p></body></html>";
+	}
+  @RequestMapping("*")
+  public String notFound() {
+    return "<!DOCTYPE html><html><head><title>Error</title></head><body><p>This page doesn't exist.</p></body></html>";
+  }
+}
